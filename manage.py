043a71fffd05d1277656c1ac257d18d6d2e4a5ad
hhy-home <1014596312@qq.com>
2017-06-28 00:14:25 +0800

    move pymysql from manage.py to __init__

--- manage.py
+++ manage.py
@@ -4,7 +4,7 @@ import sys
 import pymysql
 import django
 
-pymysql.install_as_MySQLdb()
+# pymysql.install_as_MySQLdb()
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
     django.setup()
